63007e18cf045e5863eff454ebd9b663d2b7c7b6
radoshDev <76726401+radoshDev@users.noreply.github.com>
2021-12-30 23:42:38 +0200

    Revert "Codereview"

--- src/features/trends/trendsAPI.ts
+++
@@ -1,9 +0,0 @@
-import { Trends } from "types/trendsTypes"
-import { API } from "utils/API"
-
-export class TrendsAPI {
-	static async getFeed() {
-		const response = await API.get<Trends>("/trending/feed")
-		return response.data
-	}
-}
